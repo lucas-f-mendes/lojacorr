ce2fa96b53a2ab9af220dc7a71072b62bdfc20b8
Lucas Mendes <lucas.mendes@phonetrack.com.br>
2024-08-06 11:06:31 -0300

    [new] routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,38 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\SubcategoryController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::prefix("v1")->group(function () {
+    Route::controller(AuthController::class)->group(function () {
+        Route::post("authentication/login", "login")->name("api.auth.login");
+    });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::middleware("auth")->group(function () {
+        Route::controller(AuthController::class)->group(function () {
+            Route::prefix("authentication")->group(function () {
+                Route::get("logout", "logout")->name("api.auth.logout");
+                Route::get("me", "me")->name("api.auth.me");
+                Route::get("validate", "validateToken")->name("api.auth.validate");
+            });
+        });
+
+        Route::controller(CategoryController::class)->group(function () {
+            Route::get("categories", "index")->name("api.category.index");
+            Route::get("categories/{id}", "show")->name("api.category.show");
+            Route::post("categories", "create")->name("api.category.create");
+            Route::delete("categories/{id}", "destroy")->name("api.category.destroy");
+            Route::match(["patch", "put"], "categories/{id}", "update")->name("api.category.update");
+        });
+
+        Route::prefix("categories/{cat_id}")->group(function () {
+            Route::controller(SubcategoryController::class)->group(function () {
+                Route::post("subcategories", "create")->name("api.subcategory.create");
+                Route::delete("subcategories/{id}", "destroy")->name("api.subcategory.destroy");
+                Route::match(["patch", "put"], "subcategories/{id}", "update")->name("api.subcategory.update");
+            });
+        });
+    });
 });
